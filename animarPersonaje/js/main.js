47165f761ac74b97a44c0bb3f1eb26ad7a92f61b
Lennin666 <soporte@allnexus.com.co>
2019-06-07 18:27:48 -0500

    animando personaje

---
+++ animarPersonaje/js/main.js
@@ -0,0 +1,39 @@
+console.log("Entro al JS");
+
+setTimeout(() => {
+  startAnimation();
+}, 3000);
+
+function startAnimation() {
+  // alert("hola");
+  
+  document.getElementById("personaje").style.MsAnimationPlayState = "running";
+  document.getElementById("personaje").style.webkitAnimationPlayState = "running";
+  document.getElementById("personaje").style.MozAnimationPlayState = "running";
+  document.getElementById("personaje").style.OAnimationPlayState = "running";
+  document.getElementById("personaje").style.animationPlayState = "running";  
+  setTimeout(() =>{
+    saludar();
+  },2000);  
+}
+
+function StartFondo() {
+  document.getElementById("fondo").style.MsAnimationPlayState = "running";
+  document.getElementById("fondo").style.webkitAnimationPlayState = "running";
+  document.getElementById("fondo").style.MozAnimationPlayState = "running";
+  document.getElementById("fondo").style.OAnimationPlayState = "running";
+  document.getElementById("fondo").style.animationPlayState = "running";  
+}
+
+var saludo = document.getElementById("saludo");
+
+function saludar() {
+  console.log("Estoy en saludar");
+  saludo.muted = false;
+
+}
+
+// function boton() {
+//  // alert("presiono el boton");
+//   document.location.href("/html/saludo.html");
+// }
